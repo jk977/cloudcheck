2ebdb78f197daed64a2282b3f4b834ddc43fd046
jk977 <jdk64@comcast.net>
2021-01-02 20:30:01 -0800

    Refactor sshd.rs for readability, concision

--- src/sshd.rs
+++ src/sshd.rs
@@ -1,36 +1,49 @@
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 
-/**
- * Get the `n`th field in `s`, using whitespace as a delimiter.
- */
-fn get_field(s: &str, n: usize) -> Option<&str> {
-    s.split_whitespace().nth(n)
-}
-
-/**
- * Check if `log` is an sshd log.
- */
-fn log_is_sshd(log: &str) -> bool {
-    const SERVICE_IDX: usize = 4;
-    get_field(log, SERVICE_IDX)
-        .map(|s| s.starts_with("sshd["))
-        .unwrap_or(false)
-}
-
-/**
- * Check if `log` is an sshd login failure.
- */
-fn log_is_sshd_failure(log: &str) -> bool {
-    const MSG_START_IDX: usize = 5;
-    log_is_sshd(log) && get_field(log, MSG_START_IDX) == Some("Invalid")
-}
+const SERVICE_FIELD: usize = 4;
+const MSG_FIELD: usize = 5;
+const USER_FIELD: usize = 7;
+const ADDRESS_FIELD: usize = 9;
+const PORT_FIELD: usize = 11;
 
 pub enum LogError {
     BadFormat,
     BadValue,
 }
 
+/**
+ * Get the `n`th field in `s`, using whitespace as a delimiter.
+ */
+fn get_field(s: &str, n: usize) -> Result<&str, LogError> {
+    s.split_whitespace().nth(n).ok_or(LogError::BadFormat)
+}
+
+/**
+ * Parse the `n`th field in `s`, using whitespace as a delimiter.
+ */
+fn parse_field<T: FromStr>(s: &str, n: usize) -> Result<T, LogError> {
+    get_field(s, n)?.parse().map_err(|_| LogError::BadValue)
+}
+
+/**
+ * Check if the `n`th field in `s` passes the predicate `f`.
+ */
+fn check_field<F>(s: &str, n: usize, f: F) -> bool
+    where F: Fn(&str) -> bool
+{
+    get_field(s, n).map(f).unwrap_or(false)
+}
+
+/**
+ * Check if `log` is an sshd authentication failure.
+ */
+fn log_is_sshd_failure(log: &str) -> bool {
+    check_field(log, SERVICE_FIELD, |s| s.starts_with("sshd["))
+        && check_field(log, MSG_FIELD, |s| s == "Invalid")
+}
+
+#[derive(Debug)]
 pub struct SshdEvent {
     pub log: String,
     pub user: String,
@@ -42,25 +55,15 @@ impl FromStr for SshdEvent {
     type Err = LogError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        use LogError::*;
-
         if !log_is_sshd_failure(s) {
-            return Err(BadFormat);
+            return Err(LogError::BadFormat);
         }
 
         Ok(SshdEvent {
             log: s.to_string(),
-            user: get_field(s, 7)
-                .ok_or(BadFormat)?
-                .to_string(),
-            addr: get_field(s, 9)
-                .ok_or(BadFormat)?
-                .parse()
-                .map_err(|_| BadValue)?,
-            port: get_field(s, 11)
-                .ok_or(BadFormat)?
-                .parse()
-                .map_err(|_| BadValue)?,
+            user: get_field(s, USER_FIELD)?.to_string(),
+            addr: parse_field(s, ADDRESS_FIELD)?,
+            port: parse_field(s, PORT_FIELD)?,
         })
     }
 }
